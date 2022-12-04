8ba16ca4911e07a69a190d3a4b6cf9b2ff1aef69
dev-leticia-costa <leticiaccmarques@hotmail.com>
2022-12-04 15:40:00 -0300

    First commit

---
+++ src/main/java/com/dio/desafioifood/DesafioIfoodApplication.java
@@ -0,0 +1,13 @@
+package com.dio.desafioifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIfoodApplication.class, args);
+	}
+
+}
